edf818839fe9666a2591c1791261a205b4077f79
Rasa <rasa.zukauskaite@gmail.com>
2024-09-11 10:25:13 +0300

    lesson update

---
+++ client/src/data/locationsData.js
@@ -0,0 +1,39 @@
+import img1 from '../assets/locations/1.jpg';
+import img2 from '../assets/locations/2.jpg';
+import img3 from '../assets/locations/3.jpg';
+
+export const locationsData = [
+    {
+        img: img1,
+        name: 'Menulis',
+        address: {
+            country: 'Kosmosas',
+            city: 'A',
+            street: 'A',
+            number: 'A',
+            zip: 'A',
+        },
+    },
+    {
+        img: img2,
+        name: 'Gelyte',
+        address: {
+            country: 'Tundra',
+            city: 'B',
+            street: 'B',
+            number: 'B',
+            zip: 'B',
+        },
+    },
+    {
+        img: img3,
+        name: '',
+        address: {
+            country: 'Jura',
+            city: 'C',
+            street: 'C',
+            number: 'C',
+            zip: 'C',
+        },
+    },
+];
