c25e0ef08d993fe4e1a1b4265d2995d6cebcf483
Rasa <rasa.zukauskaite@gmail.com>
2024-09-16 10:59:19 +0300

    more info

--- client/src/pages/LocationListing.jsx
+++ client/src/pages/LocationListing.jsx
@@ -1,12 +1,26 @@
+import { useState, useEffect } from "react";
 import { Footer } from "../components/footer/Footer";
 import { Header } from "../components/header/Header";
 import { LocationCard } from "../components/locations/LocationCard";
 
-
-
-
 export function LocationListing() {
-  const locationsData = [];
+  const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5020/api/locations')
+            .then(res => res.json())
+            .then(obj => {
+                if (typeof obj !== 'object') {
+                    throw new Error('Is serverio atejo ne objektas');
+                } else {
+                    setLocations(obj.data);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
+    
     return (
     <>
       <Header/>
@@ -15,14 +29,14 @@ export function LocationListing() {
             <div className="row">
                 <div className="col-12">
                     <h1>Lankytinos vietos</h1>
-                    <p>Peržiūrėk ir išsirink vietas, kurias onrėtum aplankyti</p>
+                    <p>Peržiūrėk ir išsirink vietas, kurias norėtum aplankyti</p>
                 </div>
             </div>
         </div>
 
         <div className="container px-4 py-5">
           <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-            {locationsData.map((location, index) => 
+            {locations.map((location, index) => 
               <LocationCard key={index} {...location} />)}
           </div>
         </div>
--- server/router/locations/locations.js
+++ server/router/locations/locations.js
@@ -1,4 +1,4 @@
-import connection from '../../db';
+import { connection } from '../../db.js';
 import express from 'express';
 
 export const locationsAPIrouter = express.Router();
@@ -6,49 +6,11 @@ export const locationsAPIrouter = express.Router();
 locationsAPIrouter.get('/', getLocations);
 
 async function getLocations(req, res) {
-    const data = [
-        {
-            img: '/',
-            name: 'Menulis',
-            address: {
-                country: 'Kosmosas',
-                city: 'A',
-                street: 'A',
-                number: 'A',
-                zip: 'A',
-            },
-        },
-        {
-            img: '/',
-            name: 'Gelyte',
-            address: {
-                country: 'Tundra',
-                city: 'B',
-                street: 'B',
-                number: 'B',
-                zip: 'B',
-            },
-        },
-        {
-            img: '/',
-            name: '',
-            address: {
-                country: 'Jura',
-                city: 'C',
-                street: 'C',
-                number: 'C',
-                zip: 'C',
-            },
-        },
-    ];
-
     const sql = 'SELECT * FROM locations;';
-    const dataFromServer = await connection.execute();
-
-    console.log(dataFromServer);
+    const dataFromServer = await connection.execute(sql);
 
     return res.json({
         status: 'success',
-        data: data,
+        data: dataFromServer[0],
     });
-};
+}
